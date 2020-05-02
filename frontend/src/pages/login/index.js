9a6f207a636b6f866e3148d5c3b2e493559010a8
wlborges <wellingtonborges94@hotmail.com>
2020-05-02 15:36:22 -0300

    login page

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaSignInAlt} from 'react-icons/fa';
 
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
@@ -6,8 +7,18 @@ import logoImg from '../../assets/logo.svg';
 export default function Login(){
     return(
         <div className="login-container">
-            <h1>Amigo Chocolate</h1>
+            <h1>Amigo Chocolate teste</h1>
             <img src={logoImg} alt="logo"/>
+            <form>
+                <input placeholder="Email"/>
+                <input placeholder="Senha" type="password" />
+
+                <button className="button" type='submit'>Login</button>
+
+                <a href="/cadastro">
+                    <FaSignInAlt size={16} color="#E5E5E5" />
+                    Não tenho cadastro</a>
+            </form>
 
         </div>
     );
