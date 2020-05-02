79055288b8d20d9b3089e2c494278dbb22e5a242
wlborges <wellingtonborges94@hotmail.com>
2020-05-02 16:00:19 -0300

    page login

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -7,7 +7,7 @@ import logoImg from '../../assets/logo.svg';
 export default function Login(){
     return(
         <div className="login-container">
-            <h1>Amigo Chocolate teste</h1>
+            <h1>Amigo Chocolate</h1>
             <img src={logoImg} alt="logo"/>
             <form>
                 <input placeholder="Email"/>
