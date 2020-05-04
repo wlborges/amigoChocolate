ec4f40966b114c8105dd8dd2d5c2dee7ac1a8df8
wlborges <wellingtonborges94@hotmail.com>
2020-05-03 21:45:39 -0300

    Updates

---
+++ precache-manifest.3d1c675041ebcb591be6b8d56a2db9e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d38d7d8f359fc9fc4ca03c7c2294cfc",
+    "url": "/amigoChocolate/index.html"
+  },
+  {
+    "revision": "1a67597e5175f886acfb",
+    "url": "/amigoChocolate/static/css/2.e305b35c.chunk.css"
+  },
+  {
+    "revision": "039d48a664c962539b87",
+    "url": "/amigoChocolate/static/css/main.5d19cf2e.chunk.css"
+  },
+  {
+    "revision": "1a67597e5175f886acfb",
+    "url": "/amigoChocolate/static/js/2.f0530779.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/amigoChocolate/static/js/2.f0530779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039d48a664c962539b87",
+    "url": "/amigoChocolate/static/js/main.343b78fd.chunk.js"
+  },
+  {
+    "revision": "2bf2402d2f519010340d",
+    "url": "/amigoChocolate/static/js/runtime-main.3c7b32a3.js"
+  },
+  {
+    "revision": "40ea1265ed501d89be320eea66804eb7",
+    "url": "/amigoChocolate/static/media/logo.40ea1265.svg"
+  }
+]);
