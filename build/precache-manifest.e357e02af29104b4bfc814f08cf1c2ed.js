47d95d1a51a20817354bc457e764414a9228835d
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2020-06-20 12:55:57 -0300

    correção para netlify

---
+++ build/precache-manifest.e357e02af29104b4bfc814f08cf1c2ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c156b3f4eb70afe0110cd6c47080630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "618ad98d171f9515a5b2",
+    "url": "/static/css/2.f6d43350.chunk.css"
+  },
+  {
+    "revision": "8bf42ee34a8b9a29a1bf",
+    "url": "/static/css/main.13dde167.chunk.css"
+  },
+  {
+    "revision": "618ad98d171f9515a5b2",
+    "url": "/static/js/2.b8ad6e47.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b8ad6e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf42ee34a8b9a29a1bf",
+    "url": "/static/js/main.6bd8a853.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "65e2c88510c22cbc10ba73e9528b4d5d",
+    "url": "/static/media/icone.65e2c885.svg"
+  },
+  {
+    "revision": "40ea1265ed501d89be320eea66804eb7",
+    "url": "/static/media/logo.40ea1265.svg"
+  }
+]);
--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,10 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Link, useHistory } from 'react-router-dom';
-<<<<<<< HEAD
 import { FaTrash, FaCalendarAlt, FaCheck, FaUser, FaPlus, FaUserFriends, FaUserPlus, FaMoneyBillWave, FaTimes, FaUserTimes, FaHeart, FaRandom, FaUndo, FaEye, FaEyeSlash} from 'react-icons/fa';
-=======
-import { FaTrash, FaCalendarAlt, FaCheck, FaUser, FaPlus, FaUserFriends, FaUserPlus, FaMoneyBillWave, FaTimes} from 'react-icons/fa';
->>>>>>> parent of a701131... details
 import Header from '../../components/header';
 import './styles.css';
 import api from '../../services/api';
@@ -45,7 +41,7 @@ export default function Groups(){
             setGrupos(response.data);
             setParticipantes(response.data.participantes)
         })
-    }, [localStorage.token]);
+    }, []);
 
     async function atualizar(){
         const returnGrupo = await api.get('/grupo/'+id, config);
@@ -278,8 +274,8 @@ export default function Groups(){
                         <FaUserPlus size={25} onClick={() => setPopupAddPart(true)}/>
                         </div>
                     <hr className="line"/>
+                    <ul>
                     {participantes.map((participante, i) => 
-<<<<<<< HEAD
                         <li key={i} data-tip={participante.listaDesejos}>
                             <ReactTooltip effect="solid" multiline="true" place="bottom"/>
                             {(participante.email === localStorage.email) ? salvaAmigo(participante.amigo):""}
@@ -299,16 +295,6 @@ export default function Groups(){
                         )}
                         
                     </ul>
-=======
-                        <div key={i} className="participante">
-                            <div className="close">
-                                <FaTimes/>
-                            </div>
-                            <FaUser size="60"/><br />
-                            <strong>{participante.nome}</strong><br />{participante.email}
-                        </div>
-                    )}
->>>>>>> parent of a701131... details
                 </div>
             </div>
             <ToastContainer/>
